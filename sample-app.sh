d98115045d75b97405da2c1a5145446ec1b7d2f8
josuen56 <antony.n.escudero14@gmail.com>
2026-06-04 22:02:16 +0000

    Committing sample-app files.

---
+++ sample-app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+rm -rf tempdir
+docker rm -f samplerunning 2>/dev/null 
+
+mkdir tempdir 
+mkdir tempdir/templates 
+mkdir tempdir/static 
+cp sample_app.py tempdir/
+cp -r templates/* tempdir/templates/
+cp -r static/* tempdir/static/
+
+echo "FROM python" > tempdir/Dockerfile
+echo "RUN pip install --progress-bar off flask" >> tempdir/Dockerfile
+echo "COPY ./static /home/myapp/static/ ">> tempdir/Dockerfile
+echo "COPY ./templates /home/myapp/templates/ ">> tempdir/Dockerfile
+echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
+cd tempdir 
+docker build -t sampleapp:latest .
+docker run --privileged -t -d -p 8080:8080 --name samplerunning sampleapp 
